0b201501ba4efaa06d61eed44dadff68483c7abf
Brian Nenninger <bnenning@gmail.com>
2022-04-09 13:24:29 -0400

    Support post-2012 hardware and OS versions
    - Preferences button
    - Pause game with back button
    - Material theme if available

--- app/src/main/java/com/dozingcatsoftware/dodge/DodgeAbout.java
+++ app/src/main/java/com/dozingcatsoftware/dodge/DodgeAbout.java
@@ -2,7 +2,9 @@ package com.dozingcatsoftware.dodge;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Window;
+import android.widget.TextView;
 
 public class DodgeAbout extends Activity  {
 
@@ -12,5 +14,15 @@ public class DodgeAbout extends Activity  {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.about);
+
+        TextView tv = findViewById(R.id.aboutTextView);
+        // Padding based on screen
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int padding = Math.min(metrics.widthPixels, metrics.heightPixels) / 25;
+        tv.setPadding(padding, padding, padding, padding);
+
+        // Use larger text on physically larger screens.
+        float widthInches = metrics.widthPixels / metrics.xdpi;
+        tv.setTextSize(widthInches > 4 ? 18f : 14f);
     }
 }
